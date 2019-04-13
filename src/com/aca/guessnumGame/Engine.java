cb28bbe5f46953c251ac44bae11c28d608891e20
ashothayrapetyane <ashot.hayrapetyan.e@gmail.com>
2019-04-13 23:40:53 +0400

    GuessGame version  2.0

--- src/com/aca/guessnumGame/Engine.java
+++ src/com/aca/guessnumGame/Engine.java
@@ -6,12 +6,12 @@ public class Engine {
     static int count=0;
     private int randomInt;
     Engine(){
-        this.randomInt=(new Random()).nextInt(100);
+        this.randomInt=(new Random()).nextInt(10);
     }
 
     public StringEnum isGuessed(int guessNum){
         count++;
-        return (randomInt==guessNum)? StringEnum.EQUAL:(randomInt>guessNum)? StringEnum.MORE: StringEnum.LESS;
+        return (this.randomInt==guessNum)? StringEnum.EQUAL:(randomInt<guessNum)? StringEnum.MORE: StringEnum.LESS;
 
     }
 }
--- src/com/aca/guessnumGame/GuessGame.java
+++ src/com/aca/guessnumGame/GuessGame.java
@@ -13,13 +13,13 @@ public class GuessGame {
             int guess = (new Scanner(System.in)).nextInt();
             switch (game.isGuessed(guess)) {
                 case MORE:
-                    System.out.println("More then the number you must to guess, try again!");
+                    System.out.println("Bigger then the number you must to guess, try again!");
                     break;
                 case LESS:
                     System.out.println("Less then the number you must to guess, try again!");
                     break;
                 case EQUAL:
-                    System.out.println("You are guess in the " +Engine.count+ " steps. The number is "+guess+".");
+                    System.out.println("You are guess in " +Engine.count+ " steps. The number is "+guess+".");
                     System.out.println("Congrats!!!");
                     break outer;
 
