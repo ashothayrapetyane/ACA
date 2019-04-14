3fb96eed4f44ffb465c56ce2fab8585e64f4b7ab
ashothayrapetyan <ashot.hayrapetyan.e@gmail.com>
2019-04-14 17:50:31 +0400

    encapsulating guessnumGame

--- src/com/aca/guessnumGame/Engine.java
+++ src/com/aca/guessnumGame/Engine.java
@@ -3,15 +3,42 @@ package com.aca.guessnumGame;
 import java.util.*;
 
 public class Engine {
-    static int count=0;
+    private static int counter = 0;
     private int randomInt;
-    Engine(){
-        this.randomInt=(new Random()).nextInt(10);
+
+    Engine() {
+
+        this.randomInt = (new Random()).nextInt(10);
     }
 
-    public StringEnum isGuessed(int guessNum){
-        count++;
-        return (this.randomInt==guessNum)? StringEnum.EQUAL:(randomInt<guessNum)? StringEnum.MORE: StringEnum.LESS;
+    private StringEnum isGuessed(int guessNum) {
+        counter++;
+        return (this.randomInt == guessNum) ? StringEnum.EQUAL : (randomInt < guessNum) ? StringEnum.MORE : StringEnum.LESS;
+
+    }
+
+    public void play() {
+
+        System.out.print("Guess the number: ");
+        outer:
+        do {
+
+            int guess = (new Scanner(System.in)).nextInt();
+            switch (this.isGuessed(guess)) {
+                case MORE:
+                    System.out.println("Bigger then the number you must to guess, try again!");
+                    break;
+                case LESS:
+                    System.out.println("Less then the number you must to guess, try again!");
+                    break;
+                case EQUAL:
+                    System.out.println("You are guess in " + Engine.counter + " steps. The number is " + guess + ".");
+                    System.out.println("Congrats!!!");
+                    break outer;
+
+            }
+
+        } while (true);
 
     }
 }
